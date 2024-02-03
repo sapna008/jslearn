afafb5b7fb4b27fe5fe559d2d1328dc127ba1748
crzygautam <crzygautam@gmail.com>
2024-02-03 11:51:50 +0530

    js

---
+++ TODOAPP/todo.js
@@ -0,0 +1,20 @@
+var data = [];
+function addData(e){
+    // to get input data from input box and add to global variable data
+
+
+
+    //call display function to display all added data.
+}
+
+function display(){
+    // takes global data and itrate over it and then render to ui
+}
+
+function showErrorMsg(msg){
+    // method to display all type errors on ui
+}
+
+function showSuccessMsg(msg){
+    // method to display to show success msg on ui
+}

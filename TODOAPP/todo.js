91cfae75f47e7d805752d5f7a088e457b6b75fe5
sapna sarkar <sapnapks1@gmail.com>
2024-02-03 15:24:53 +0530

    jdfhgjdh

--- TODOAPP/todo.js
+++ TODOAPP/todo.js
@@ -1,5 +1,6 @@
 var data = [];
-function addData(e){
+
+function addData(e) {
     // to get input data from input box and add to global variable data
 
 
@@ -7,14 +8,34 @@ function addData(e){
     //call display function to display all added data.
 }
 
-function display(){
+function display() {
     // takes global data and itrate over it and then render to ui
+    // step 1 : target tbody of your table by Id and assign to a varibale . eg. let tableBody = document.ge.....
+    // step 2 : crete element tr, and ( td as required). 
+    // step three add tds to tr then this tr to tbody
+    //  for reference : 
+    // const imageWrapper = document.createElement("div");
+    //     imageWrapper.classList.add("search-result");
+    //     const image = document.createElement("img");
+    //     image.src = result.urls.small;
+    //     image.alt = result.alt_description;
+    //     const imageLink = document.createElement('a');
+    //     // imageLink.href = result.links.html;
+    //     //imageLink.target = "_blank";
+    //     imageLink.textContent = result.alt_description;
+    //     imageWrapper.appendChild(image);
+    //     imageWrapper.appendChild(imageLink);
+    //     searchResults.appendChild(imageWrapper);
+
+    //  write your logic here.
+    let tab_body = document.getElementById('table_body')
+
 }
 
-function showErrorMsg(msg){
+function showErrorMsg(msg) {
     // method to display all type errors on ui
 }
 
-function showSuccessMsg(msg){
+function showSuccessMsg(msg) {
     // method to display to show success msg on ui
 }

602b80b238dc390f1d444617683f68fa1717dc98
sapna sarkar <sapnapks1@gmail.com>
2024-02-03 17:20:58 +0530

    vbxvb

--- TODOAPP/todo.js
+++ TODOAPP/todo.js
@@ -1,75 +1,33 @@
-var data = [{
-        "id": 1,
-        "firstName": "Terry",
-        "lastName": "Medhurst",
-        "maidenName": "Smitham",
-        "age": 50,
-        "gender": "male",
-        "email": "atuny0@sohu.com",
-        "phone": "+63 791 675 8914",
-        "username": "atuny0",
-        "password": "9uQFF1Lh",
-        "birthDate": "2000-12-25",
-        "image": "https://robohash.org/Terry.png?set=set4",
-        "address": {
-            "address": "1745 T Street Southeast",
-            "city": "Washington",
-            "coordinates": {
-                "lat": 38.867033,
-                "lng": -76.979235
-            },
-            "postalCode": "20020",
-            "state": "DC"
-        }
-    },
-    {
-        "id": 2,
-        "firstName": "Jon",
-        "lastName": "Doe",
-        "maidenName": "",
-        "age": 19,
-        "gender": "male",
-        "email": "atkut0@sohu.com",
-        "phone": "+63 791 675 8914",
-        "username": "Jondoe1",
-        "password": "9uQFF1Lh",
-        "birthDate": "2000-12-25",
-        "image": "https://robohash.org/Terry.png?set=set4",
-        "address": {
-            "address": "1745 T Street Southeast",
-            "city": "Washington",
-            "coordinates": {
-                "lat": 38.867033,
-                "lng": -76.979235
-            },
-            "postalCode": "20020",
-            "state": "DC"
-        }
-    },
-
-];
+var data = [];
 
 function AddUser() {
     // Create an object to store form data
-    var userData = {};
+    let userData = {};
 
     // Get form data and populate the object
-    userData.username = document.getElementById("username").value;
     userData.firstName = document.getElementById("firstName").value;
     userData.lastName = document.getElementById("lastName").value;
     userData.age = document.getElementById("age").value;
 
     // Get gender value
-    var gender = document.querySelector('input[name="gender"]:checked');
+    let gender = document.querySelector('input[name="gender"]:checked');
+    console.log('gender ', gender);
     userData.gender = gender ? gender.value : null;
 
-    userData.phoneNumber = document.getElementById("phoneNumber").value;
+    userData.phone = document.getElementById("phoneNumber").value;
     userData.birthday = document.getElementById("birthday").value;
-    userData.location = document.getElementById("location").value;
+    userData.address = { city: document.getElementById("location").value }
     userData.email = document.getElementById("email").value;
+    userData.username = document.getElementById("username").value;
+    if (userData.username == "") {
+        userData.username = userData.email;
 
+    }
     // Log or perform actions with the form data object
     console.log("User Data:", userData);
+    data.push(userData)
+    console.log(" Data:", data);
+    display()
 
 }
 
@@ -94,9 +52,66 @@ function display() {
     //     searchResults.appendChild(imageWrapper);
 
     //  write your logic here.
+    let tbody = document.getElementById('tableBody')
+    tbody.innerHTML = ''
+    let index = 1;
+    data.forEach(function(item) {
+        const row = document.createElement("tr")
+        const colhead = document.createElement("th")
+        colhead.scope = "row"
+        colhead.innerText = index;
+        index++;
+        row.appendChild(colhead)
+
+        //td
+        const t1 = document.createElement("td")
+        t1.innerText = item.firstName
+        row.appendChild(t1)
+
+        const t2 = document.createElement("td")
+        t2.innerText = item.lastName
+        row.appendChild(t2)
+        const t3 = document.createElement("td")
+        t3.innerText = item.age
+        row.appendChild(t3)
+        const t4 = document.createElement("td")
+        t4.innerText = item.gender
+        row.appendChild(t4)
+        const t5 = document.createElement("td")
+        t5.innerText = item.phone
+        row.appendChild(t5)
+        const t6 = document.createElement("td")
+        t6.innerText = item.birthDate
+        row.appendChild(t6)
+        const t8 = document.createElement("td")
+        t8.innerText = item.address.city
+        row.appendChild(t8)
+        const t7 = document.createElement("td")
+        t7.innerText = item.email
+        row.appendChild(t7)
+
+        const t9 = document.createElement("td")
+        const edit = document.createElement("button")
+        const delet = document.createElement("button")
+        delet.classList.add("btn", "btn-primary");
+        edit.classList.add("btn", "btn-primary");
+        edit.innerHTML = "edit &nbsp;"
+
+        delet.innerText = "delete"
+        t9.appendChild(edit)
+        t9.appendChild(delet)
+        row.appendChild(t9)
+
+
+
+        tbody.appendChild(row)
+
+
+    })
 
 
 }
+display()
 
 function showErrorMsg(msg) {
     // method to display all type errors on ui
